4da893ff32e63489eb1c239a1152b4f3367d0425
Juan Camilo Fernandez Salinas <jkfs.jkfs@gmail.com>
2023-03-27 00:51:41 -0500

    ....

---
+++ Circuito.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1
+{
+    class Circuito
+    {
+        string Nombre { get; set; }
+        int Vueltas { get; set; }
+        Monoplaza Monoplaza { get; set; }
+
+        Dictionary<Monoplaza, long> Posiciones = new Dictionary<Monoplaza, long>();     
+
+        public Circuito(string cNombre, int nVueltas)
+        {
+            Nombre = cNombre;
+            Vueltas = nVueltas;
+        }
+
+        public void AgregarMonoPlaza(Monoplaza car)
+        {
+            if (this.Monoplaza == null)
+                this.Monoplaza = car;
+        }
+
+        public void SacarMonoPlaza(Monoplaza car)
+        {
+            if (this.Monoplaza != null)
+                this.Monoplaza = null;
+        }
+
+
+        public void RealizarPrueba() 
+        {
+            this.Monoplaza.Encender();
+            long nBestTime = 0;
+            Console.WriteLine($"Iniciando prueba Monoplaza {this.Monoplaza.Nombre} - {this.Monoplaza.Escuderia}");
+            this.Monoplaza.Movimiento();
+            for (int i = 1; i <= this.Vueltas; i++)
+            {
+                long nTime = new Random(i).Next(100000, 999999);
+                Console.WriteLine($"Vuelta {i} - Tiempo: {nTime}");
+                if(nTime < nBestTime)
+                    nBestTime = nTime;
+            }
+
+            Console.WriteLine($"Finalizada prueba Monoplaza {this.Monoplaza.Nombre} - {this.Monoplaza.Escuderia}");
+            Console.WriteLine($"Mejor tiempo: {nBestTime}");
+
+            this.Monoplaza.Apagar();
+            this.Posiciones.Add(this.Monoplaza, nBestTime);
+        }
+
+
+        public void Finalizar()
+        {
+            Console.WriteLine($"Circuito {this.Nombre} Finalizado.");
+            Console.WriteLine($"Tabla de Posiciones:");
+
+            this.Posiciones.OrderBy(p => p.Value);
+
+            int nPos = 1;
+            foreach (var item in this.Posiciones)
+            {
+                Console.WriteLine($"{nPos}. {item.Key.Nombre.Trim().PadRight(20)} Tiempo:{item.Value}");
+                nPos++;
+            }
+        }
+    }
+}
---
+++ Monoplaza.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1
+{
+    abstract class Monoplaza
+    {
+        public string Escuderia { get; set; }
+        public string Nombre { get; set; }
+        bool start, moving;
+
+        public void Encender() 
+        { 
+            if (!start)
+            {
+                start = true;
+                Console.WriteLine($"Vehiculo {Nombre} encendido.");
+            }
+            else
+                Console.WriteLine($"El Vehiculo {Nombre} ya se encuentra encendido.");
+        }
+
+        public void Apagar() 
+        {
+            if (start)
+            {
+                if (!moving)
+                {
+                    start = false;
+                    Console.WriteLine($"Vehiculo {Nombre} apagado.");
+                }
+                else
+                    Console.WriteLine($"No se puede apagar el Vehiculo {Nombre}, está en movimiento.");
+            }
+            else
+                Console.WriteLine($"El Vehiculo {Nombre} ya se encuentra apagado.");
+        }
+
+        public void Detener() 
+        {
+            if (start)
+            {
+                if (moving)
+                {
+                    moving = false;
+                    Console.WriteLine($"Vehiculo {Nombre} detenido.");
+                }
+                else
+                    Console.WriteLine($"El Vehiculo {Nombre}, NO está en movimiento.");
+            }
+            else
+                Console.WriteLine($"El Vehiculo {Nombre} se encuentra apagado.");
+        
+        }
+
+        public void Movimiento() 
+        {
+            if (start)
+            {
+                if (!moving)
+                {
+                    moving = true;
+                    Console.WriteLine($"Vehiculo {Nombre} puesto en movimiento.");
+                }
+                else
+                    Console.WriteLine($"El Vehiculo {Nombre}, Ya se encuentra en movimiento.");
+            }
+            else
+                Console.WriteLine($"El Vehiculo {Nombre} se encuentra apagado.");
+        
+        }
+
+    }
+}
